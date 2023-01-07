d0e1decd57d520e5352d579a7e7672d6dfe52789
Shetty02 <shettynehal87@gmail.com>
2023-01-07 11:53:38 +0530

    Follower & Following List & Unfollow part Done

--- Controllers/Follow.js
+++ Controllers/Follow.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { followerUser } = require("../Models/Follow");
+const { followerUser, followingUserList, followerUserList, unfollowUser } = require("../Models/Follow");
 const FollowRouter = express.Router();
 const User = require("../Models/User")
 
@@ -48,4 +48,131 @@ FollowRouter.post("/follow-user",async (req, res)=>{
         }
 })
 
+// Here we are finding whom you are following.
+FollowRouter.post("/following-list", async(req, res)=>{
+    const followerUserId = req.session.user.userId;
+    const offset = req.query.offset || 0;
+
+    // Validating  the followeruserID.
+        try{
+            await User.verifyUserId({ userId : followerUserId });
+        }
+        catch(err){
+            return res.send({
+                status:401,
+                message:"Invalid Follower userId",
+                error: err
+            })
+        }
+        try {
+            const data = await followingUserList({followerUserId, offset});
+
+            if(data.length === 0){
+                return res.send({
+                    status:200,
+                    message:"You have not followed anyone yet 😒😎.",
+                    data: data
+                })
+            }
+            return res.send({
+                status:200,
+                message:"Following List",
+                data: data
+            })
+        } catch (error) {            
+            return res.send({
+                status:400,
+                message:"Error Occured",
+                error: error,
+            })
+        }
+})
+
+// Here we are finding who follows you.
+FollowRouter.post("/followers-list", async(req, res)=>{
+    const followingUserId = req.session.user.userId;
+    const offset = req.query.offset || 0;
+
+    // Validating  the followinguserID.
+        try{
+            await User.verifyUserId({ userId : followingUserId });
+        }
+        catch(err){
+            return res.send({
+                status:401,
+                message:"Invalid Following userId",
+                error: err
+            })
+        }
+        try {
+            const data = await followerUserList({followingUserId, offset});
+
+            if(data.length === 0){
+                return res.send({
+                    status:200,
+                    message:"No one Follow you 🥹😭",
+                    data: data
+                })
+            }
+            return res.send({
+                status:200,
+                message:"Followers List",
+                data: data
+            })
+        } catch (error) {            
+            return res.send({
+                status:400,
+                message:"Error Occured",
+                error: error,
+            })
+        }
+})
+
+FollowRouter.post("/unfollow-user", async(req, res)=>{
+    const followingUserId = req.body.followingUserId;
+    const followerUserId = req.session.user.userId; 
+
+    // console.log(followingUserId);
+    // console.log(followerUserId);
+    
+    // Validating both the followers and following userID.
+    try{
+        await User.verifyUserId({ userId : followerUserId });
+    }
+    catch(err){
+        return res.send({
+            status:401,
+            message:"Invalid Follower userId",
+            error: err
+        })
+    }
+    try{
+        await User.verifyUserId({ userId : followingUserId });
+    }
+    catch(err){
+        return res.send({
+            status:401,
+            message:"Invalid Following userId",
+            error: err
+        })
+    }
+
+    try {
+        const unfollowDb = await unfollowUser({ followerUserId, followingUserId});
+        
+        return res.send({
+            status:200,
+            message:"Unfollow Successfull.",
+            data: unfollowDb
+        })
+        
+    } catch (error) {
+        return res.send({
+            status:401,
+            message:"Error Occurred",
+            error: error
+        })
+        
+    }
+})
 module.exports = FollowRouter;
--- Models/Follow.js
+++ Models/Follow.js
@@ -1,4 +1,7 @@
 const followSchema = require("../Schemas/Follow");
+const ObjectId = require("mongodb").ObjectId;
+const userSchema = require("../Schemas/User");
+const constants = require("../constants");
 
 function followerUser({followerUserId, followingUserId}){
         return new Promise(async(resolve, reject)=>{
@@ -30,5 +33,96 @@ function followerUser({followerUserId, followingUserId}){
             }
         })
 }
+function followingUserList({followerUserId, offset}){
+    return new Promise(async (resolve, reject)=>{
+        try {
+            const followDb = await followSchema.aggregate([
+                {$sort: {createDatetime : -1}}, // -1 for decreasing order.
+                { $facet : {
+                    data : [
+                        { $skip : parseInt(offset)},
+                        { $limit : constants.BLOGSLIMIT},
+                    ],
+                }},
+            ]);
+                        // resolve(blogDb[0].data)
+            
+            // .find({followerUserId});
+            
+    // Create an array followingUserId
+    let followingUserIds = [];
+    followDb[0].data.forEach((followObj)=>{
+        followingUserIds.push(ObjectId(followObj.followingUserId))
+    })
+    
+    const followingUserDetails = await userSchema.aggregate([
+        {
+            $match:{
+                _id: {
+                    $in: followingUserIds 
+                },
+            },
+        },
+    ]);
 
-module.exports = { followerUser }
+    // console.log(followingUserDetails);
+    resolve(followingUserDetails)
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+function followerUserList({followingUserId, offset}){
+    return new Promise(async (resolve, reject)=>{
+        try {
+            const followDb = await followSchema.aggregate([
+                {$sort: {createDatetime : -1}}, // -1 for decreasing order.
+                { $facet : {
+                    data : [
+                        { $skip : parseInt(offset)},
+                        { $limit : constants.BLOGSLIMIT},
+                    ],
+                }},
+            ]);
+                        // resolve(blogDb[0].data)
+            
+            // .find({followingUserId});
+            
+    // Create an array followerUserId
+    let followerUserIds = [];
+    followDb[0].data.forEach((followObj)=>{
+        followerUserIds.push(ObjectId(followObj.followerUserId))
+    })
+    
+    const followerUserDetails = await userSchema.aggregate([
+        {
+            $match:{
+                _id: {
+                    $in: followerUserIds 
+                },
+            },
+        },
+    ]);
+
+    // console.log(followerUserDetails);
+    resolve(followerUserDetails)
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+function unfollowUser({ followingUserId, followerUserId}){
+    return new Promise(async (resolve, reject)=>{
+        try {
+             const unfollowDb = await followSchema.findOneAndDelete({
+                followerUserId,
+                followingUserId
+             })           
+             resolve(unfollowDb);
+        } catch (error) {
+            reject(error);
+        }
+    })
+
+}
+module.exports = { followerUser, followingUserList, followerUserList, unfollowUser}
